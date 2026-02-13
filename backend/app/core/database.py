814a9055bdc5dce544a0c82951892efc3790fcbc
Musawenkosi <ngulubem241@gmail.com>
2026-02-13 10:09:26 +0200

    fix: Add missing 'import os' in database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 import datetime
+import os
 
 Base = declarative_base()
 
